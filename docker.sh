64afd01fd1519506000bea377b9a037d63891e48
Marshall Lee <marshall.s.lee@gmail.com>
2019-10-28 16:31:54 +0900

    edited Dockerfile

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 docker stop socketiosample && docker container rm socketiosample
 cd /opt/socketiosample
 docker build --no-cache -t socketiosample .
-docker run -d -p 12379:12379 --name=chitchat-push -ti -v /chitchat-data:/chitchat-data chitchat-push
+docker run -d -p 12379:12379 --name=socketiosample -ti -v /socketiosample:/socketiosample socketiosample
