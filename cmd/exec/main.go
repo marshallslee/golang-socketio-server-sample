96c9998ac48ee8c71d21b3e87c9d7c4172f6243d
Marshall <marshall.s.lee@gmail.com>
2019-10-30 21:24:57 +0900

    gorilla/mux

--- cmd/exec/main.go
+++ cmd/exec/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"github.com/googollee/go-socket.io"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 )
+
+var (
+	logFile      *os.File
+	socketServer *socketio.Server
+)
+
+func init() {
+	// Start logging
+	logPath := "/socketiosample/output.log"
+	logFile, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	log.Println("Start logging..")
+	log.SetOutput(logFile)
+
+	socketServer, err = socketio.NewServer(nil)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
 
 type PersonInfo struct {
 	FirstName string `json:"first_name"`
@@ -27,48 +48,35 @@ func person(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Start logging
-	logPath := "/socketiosample/output.log"
-	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
-	}
-	log.Println("Start logging..")
-	defer f.Close()
-	log.SetOutput(f)
+	defer logFile.Close()
 
-	server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server.OnConnect("/socketio", func(s socketio.Conn) error {
+	socketServer.OnConnect("/socketio", func(s socketio.Conn) error {
 		s.SetContext("")
 		log.Println("Connected:", s.ID())
 		return nil
 	})
 
-	server.OnEvent("/socketio", "name_event", func(s socketio.Conn, p PersonInfo) {
+	socketServer.OnEvent("/socketio", "name_event", func(s socketio.Conn, p PersonInfo) {
 		log.Println("First name:", p.FirstName)
 		log.Println("Last name:", p.LastName)
 	})
 
-	server.OnError("/socketio", func(e error) {
+	socketServer.OnError("/socketio", func(e error) {
 		log.Println("meet error:", e)
 	})
 
-	server.OnDisconnect("/socketio", func(s socketio.Conn, msg string) {
+	socketServer.OnDisconnect("/socketio", func(s socketio.Conn, msg string) {
 		log.Println("closed", msg)
 	})
 
-	go server.Serve()
-	defer server.Close()
+	go socketServer.Serve()
+	defer socketServer.Close()
 
-	router := http.NewServeMux()
+	router := mux.NewRouter()
 	router.HandleFunc("/hello", hello)
 	router.HandleFunc("/test", test)
-	router.HandleFunc("/person", person)
-	router.Handle("/", server)
+	router.HandleFunc("/person/{name}", person)
+	router.Handle("/", socketServer)
 
 	log.Println("Serving at localhost:12379...")
 	log.Fatal(http.ListenAndServe(":12379", router))
