17c77aefa92a155f9e2ed9918f151f5ad3187205
Marshall <marshall.s.lee@gmail.com>
2019-10-30 22:36:09 +0900

    gin

--- cmd/exec/main.go
+++ cmd/exec/main.go
@@ -51,22 +51,22 @@ func person(c *gin.Context) {
 func main() {
 	defer logFile.Close()
 
-	socketServer.OnConnect("/socketio", func(s socketio.Conn) error {
+	socketServer.OnConnect("/", func(s socketio.Conn) error {
 		s.SetContext("")
 		log.Println("Connected:", s.ID())
 		return nil
 	})
 
-	socketServer.OnEvent("/socketio", "name_event", func(s socketio.Conn, p PersonInfo) {
+	socketServer.OnEvent("/", "name_event", func(s socketio.Conn, p PersonInfo) {
 		log.Println("First name:", p.FirstName)
 		log.Println("Last name:", p.LastName)
 	})
 
-	socketServer.OnError("/socketio", func(e error) {
+	socketServer.OnError("/", func(e error) {
 		log.Println("meet error:", e)
 	})
 
-	socketServer.OnDisconnect("/socketio", func(s socketio.Conn, msg string) {
+	socketServer.OnDisconnect("/", func(s socketio.Conn, msg string) {
 		log.Println("closed", msg)
 	})
 
@@ -76,6 +76,6 @@ func main() {
 	router.GET("/hello", hello)
 	router.GET("/test", test)
 	router.GET("/person/:name", person)
-	router.GET("/", gin.WrapH(socketServer))
+	router.GET("/socketio", gin.WrapH(socketServer))
 	router.Run(":12379")
 }
